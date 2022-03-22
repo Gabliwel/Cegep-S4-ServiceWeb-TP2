8921b0ee9176dd3e67a0437f70353e67e3feac1b
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2022-03-22 17:59:39 -0400

    ajout validation pour rating, correction checkstyles et correction validation event et trail

--- src/main/java/ca/csfoy/servicesweb/camarchedoc/api/RatingDto.java
+++ src/main/java/ca/csfoy/servicesweb/camarchedoc/api/RatingDto.java
@@ -1,10 +1,24 @@
 package ca.csfoy.servicesweb.camarchedoc.api;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+import ca.csfoy.servicesweb.camarchedoc.api.validation.CreateGroupValidation;
+
 public class RatingDto {
 
+    public static final String ID_VALID_PATTERN = "[0-9]+";
+    public static final String INVALID_ID_MESSAGE = "Id must be numbers only.";
+
+    @Pattern(regexp = ID_VALID_PATTERN, message = INVALID_ID_MESSAGE, groups = {Default.class})
     public final String id;
+    @NotNull (message = "Trail must not be null.", groups = {Default.class, CreateGroupValidation.class})
     public final TrailDto trail;
     public final Double note;
+    @Size(max = 140, message = "Comment must have less than 140 caracters.", groups = {Default.class, CreateGroupValidation.class})
     public final String comment;
     
     public RatingDto(String id, TrailDto trail, Double note, String comment) {
--- src/main/java/ca/csfoy/servicesweb/camarchedoc/controller/RatingController.java
+++ src/main/java/ca/csfoy/servicesweb/camarchedoc/controller/RatingController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import ca.csfoy.servicesweb.camarchedoc.api.RatingDto;
 import ca.csfoy.servicesweb.camarchedoc.api.RatingResource;
 import ca.csfoy.servicesweb.camarchedoc.controller.converter.RatingConverter;
+import ca.csfoy.servicesweb.camarchedoc.controller.validation.CustomValidator;
+import ca.csfoy.servicesweb.camarchedoc.controller.validation.CustomValidatorFactory;
 import ca.csfoy.servicesweb.camarchedoc.domain.Rating;
 import ca.csfoy.servicesweb.camarchedoc.domain.RatingRepository;
 
@@ -12,14 +14,19 @@ public class RatingController implements RatingResource {
     
     private final RatingRepository repository;
     private final RatingConverter converter;
+    private final CustomValidatorFactory validatorFactory;
     
-    public RatingController(RatingRepository repository, RatingConverter converter) {
+    public RatingController(RatingRepository repository, RatingConverter converter, CustomValidatorFactory validatorFactory) {
         this.repository = repository;
         this.converter = converter;
+        this.validatorFactory = validatorFactory;
     }
 
     @Override
     public RatingDto getById(String id) {
+        CustomValidator<RatingDto, String> validator = validatorFactory.getRatingValidator();
+        validator.validateId(id);
+        validator.verify("Rating cannot be obtained. Invalid ID format");
         return converter.convertToRatingDtoFrom(repository.getById(id));
     }
 
@@ -30,11 +37,17 @@ public class RatingController implements RatingResource {
 
     @Override
     public List<RatingDto> search(String id) {
+        CustomValidator<RatingDto, String> validator = validatorFactory.getRatingValidator();
+        validator.validateId(id);
+        validator.verify("Ratings cannot be obtained. Invalid ID format");
         return converter.convertToRatingDtoListFrom(repository.searchByTrailId(id));
     }
 
     @Override
     public RatingDto create(RatingDto dto) {
+        CustomValidator<RatingDto, String> validator = validatorFactory.getRatingValidator();
+        validator.validate(dto);
+        validator.verify("Rating cannot be created. Invalid format");
         Rating rating = repository.create(converter.convertToRatingAtCreationFrom(dto));
         return converter.convertToRatingDtoFrom(rating);
     }
